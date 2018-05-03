e07a383171047c9a78ebb4c41c64d1eb76cc258f
WangYio <247151028@qq.com>
2018-05-03 11:33:50 +0800

    统计阅读量，自动摘要

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.utils.html import strip_tags
+import markdown
 
 
 # 分类表
@@ -29,6 +31,10 @@ class Post(models.Model):
     category = models.ForeignKey(Category,on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    views = models.PositiveIntegerField(default=0) #PositiveIntegerField,该类型的值只允许为正整数或 0
+
+    class Meta:
+        ordering = ['-created_time', 'title']
 
     def __str__(self):
         return self.title
@@ -36,7 +42,22 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse('blog:detail', kwargs={'pk': self.pk})
 
-    class Meta:
-        ordering = ['-created_time','title']
+    def increase_views(self): #文章阅读量方法
+        self.views += 1
+        self.save(update_fields=['views']) #使用了 update_fields 参数来告诉 Django 只更新数据库中 views 字段的值，以提高效率。
+
+    def save(self, *args, **kwargs):
+        if not self.excerpt:
+            md = markdown.Markdown(extensions=[
+                'markdown.extensions.extra',
+                'markdown.extensions.codehilite',
+            ])
+
+            self.excerpt = strip_tags(md.convert(self.body))[:54]
+        super(Post, self).save(*args, **kwargs)
+
+
+
+
 
 
